c966065fb4c1fb20e9ca54e0d6969a59977cd187
Lucas Ramson Siefert <lucasramson@gmail.com>
2025-12-16 13:45:11 -0300

    feat: PONG

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{{
+    io::{Read, Write},
+    net::TcpListener}};
+
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+
+                let mut buffer = [0; 512];
+                stream.read(&mut buffer).unwrap();
+                println!("request: {}", String::from_utf8_lossy(&buffer[..]));
+                stream.write_all(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
